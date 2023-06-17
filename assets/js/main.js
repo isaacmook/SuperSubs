9ad91339d598283f7ec028dca608403603e7a237
Izyk02 <21100354@virtualwindow.co.za>
2023-06-17 13:14:59 +0200

    images and css
    side nav implimentation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,44 @@
-// Global variables
-var subs = []; // Array to store the created subs
+document.addEventListener('DOMContentLoaded', function(event) {
 
-// Function to add a sub
-function addSub() {
-  var subName = document.getElementById('sub-name').value;
-  var breadPrice = parseFloat(document.getElementById('bread').value);
-  var selectedToppings = document.querySelectorAll('#toppings input[type="checkbox"]:checked');
-  var selectedSauce = document.querySelector('#sauces input[type="radio"]:checked');
+  const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId)
+    
+    // Validate that all variables exist
+    if(toggle && nav && bodypd && headerpd){
+    toggle.addEventListener('click', ()=>{
+    // show navbar
+    nav.classList.toggle('show')
+    // change icon
+    toggle.classList.toggle('bx-x')
+    // add padding to body
+    bodypd.classList.toggle('body-pd')
+    // add padding to header
+    headerpd.classList.toggle('body-pd')
+    })
+    }
+    }
+    
+    showNavbar('header-toggle','nav-bar','body-pd','header')
+    
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link')
+    
+    function colorLink(){
+    if(linkColor){
+    linkColor.forEach(l=> l.classList.remove('active'))
+    this.classList.add('active')
+    }
+    }
+    linkColor.forEach(l=> l.addEventListener('click', colorLink))
+    
+     // Your code to run since DOM is loaded and ready
+    
+});
+//-----------------------
+// BUILD A SUB 
+//-------------------------------
 
-  var toppingsCost = 0;
-  for (var i = 0; i < selectedToppings.length; i++) {
-    toppingsCost += parseFloat(selectedToppings[i].value);
-  }
-
-  var sauceCost = parseFloat(selectedSauce.value);
-
-  var subCost = breadPrice + toppingsCost + sauceCost;
-
-  var sub = {
-    name: subName,
-    bread: breadPrice,
-    toppings: toppingsCost,
-    sauce: sauceCost,
-    cost: subCost
-  };
-
-  subs.push(sub);
-
-  // Update the order summary
-  updateOrderSummary();
-
-  // Reset the form
-  document.getElementById('sub-form').reset();
-}
-
-// Function to update the order summary
-function updateOrderSummary() {
-  var subList = document.getElementById('sub-list');
-  var totalCost = 0;
-
-  // Clear the existing sub list
-  subList.innerHTML = '';
-
-  // Display each sub in the list
-  subs.forEach(function(sub) {
-    var listItem = document.createElement('li');
-    listItem.textContent = sub.name + ' - $' + sub.cost.toFixed(2);
-    subList.appendChild(listItem);
-
-    totalCost += sub.cost;
-  });
-
-  // Update the total cost
-  document.getElementById('total-cost').textContent = '$' + totalCost.toFixed(2);
-}
-
-// Function to handle form submission
-function handleFormSubmission(event) {
-  event.preventDefault();
-}
-
-// Event listener for form submission
-document.getElementById('sub-form').addEventListener('submit', handleFormSubmission);
+  
