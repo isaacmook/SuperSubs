d6ff78f72450c45d2f77ee9c36202869385b9a62
Izyk02 <21100354@virtualwindow.co.za>
2023-06-18 07:31:42 +0200

    functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,90 @@
-document.addEventListener('DOMContentLoaded', function(event) {
 
-  const showNavbar = (toggleId, navId, bodyId, headerId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId),
-    bodypd = document.getElementById(bodyId),
-    headerpd = document.getElementById(headerId)
-    
-    // Validate that all variables exist
-    if(toggle && nav && bodypd && headerpd){
-    toggle.addEventListener('click', ()=>{
-    // show navbar
-    nav.classList.toggle('show')
-    // change icon
-    toggle.classList.toggle('bx-x')
-    // add padding to body
-    bodypd.classList.toggle('body-pd')
-    // add padding to header
-    headerpd.classList.toggle('body-pd')
-    })
-    }
-    }
-    
-    showNavbar('header-toggle','nav-bar','body-pd','header')
-    
-    /*===== LINK ACTIVE =====*/
-    const linkColor = document.querySelectorAll('.nav_link')
-    
-    function colorLink(){
-    if(linkColor){
-    linkColor.forEach(l=> l.classList.remove('active'))
-    this.classList.add('active')
-    }
-    }
-    linkColor.forEach(l=> l.addEventListener('click', colorLink))
-    
-     // Your code to run since DOM is loaded and ready
-    
-});
-//-----------------------
+//--------------------------------------------------
 // BUILD A SUB 
-//-------------------------------
+//--------------------------------------------------
 
+// Global variables to store order data
+let superOrder = [];
+let superSubTotal = 0
+
+// Function to add a sub
+addSub = () => {
+
+  let subName = document.getElementById("sub-name").value;
+  let breadType = document.getElementById("bread").value;
+  let howDoYouLikeIt = "";
+
+  if (breadType === "White Bread") {
+    superSubTotal += 2.99;
+  } else if (breadType === "Brown Bread") {
+    superSubTotal += 2.49;
+  } else if (breadType === "Whole Wheat Bread") {
+    superSubTotal += 3.49;
+  } else if (breadType === "Multigrain Bread") {
+    superSubTotal += 3.99;
+  }
+
+  // Declare and calculate values from the radio buttons
+  const radios = document.getElementsByName("bread-state-radios");
+  howDoYouLikeIt = Array.from(radios).find((radio) => radio.checked).value;
   
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      howDoYouLikeIt = radios[i].value
+      superSubTotal = superSubTotal + radios[i].dataset.cost
+    }
+    
+  }
+  // Declare and calculate values from the checkboxes
+  let subToppings = document.getElementsByName("toppings");
+  const subTopArray = [];
+
+  for (let i = 0; i < subToppings.length; i++) {
+      if (subToppings[i].checked) {
+          subTopArray.push(subToppings[i].value);
+          superSubTotal = superSubTotal + subToppings[i].dataset.cost
+      }
+  }
+
+  let subSause = document.getElementsByName('sauce');
+  const subSauceArray = [];
+
+  for (let i = 0; i < subSause.length; i++) {
+    if (subSause[i].checked) {
+      subSauceArray.push(subSause[i].value);
+      superSubTotal = superSubTotal + subSause[i].dataset.cost
+    }
+    
+  }
+
+  superOrder.push({
+    subName: subName,
+    bread: breadType,
+    howDoYouLikeIt: howDoYouLikeIt,
+    toppings: subTopArray,
+    sauce: subSauceArray,
+    price: superSubTotal
+  });
+
+  console.log(superOrder)
+}
+
+// Function to display the order summary
+displayOrder = () => {
+  const orderSummary = document.getElementById("order-summary");
+  orderSummary.innerHTML = "<h3>Order Summary:</h3>";
+
+  for (let i = 0; i < superOrder.length; i++) {
+    const sub = superOrder[i];
+    orderSummary.innerHTML += `<div>${sub.name} - R${sub.subTotal}</div>`;
+  }
+
+   // Calculate the total cost
+   let totalCost = 0;
+   for (let i = 0; i < superOrder.length; i++) {
+     totalCost += parseFloat(superOrder[i].subTotal);
+   }
+ 
+   // Update the total cost in the order summary
+   orderSummary.innerHTML += `<div>Total: R${totalCost.toFixed(2)}</div>`;
+ };
